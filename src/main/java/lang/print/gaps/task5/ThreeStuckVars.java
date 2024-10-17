14ec0d6c69ade4d065aa39e71905ff9187ec5339
xden78 <venera.opt@gmail.com>
2024-10-17 17:57:17 +0300

    TimeConvertor

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        // Create 3 int variables with names: first, second, third. Initialize them with values = 1, 10, 100.
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(String.valueOf(first) + second + third);
     }
 }
